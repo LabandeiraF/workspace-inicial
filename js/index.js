7cd07725cfb9109c6670934bdab1d11bdd85d598
Fernando Labandeira <nandolabandeira15@gmail.com>
2023-09-14 20:28:14 -0300

    update products.js index.js categories.js product-info.js

--- js/index.js
+++ js/index.js
@@ -1,3 +1,17 @@
+const isLoggedIn = true;
+     
+if (isLoggedIn) {
+    const username = localStorage.getItem("Email");
+    const usernameElement = document.getElementById("logged-in-username");
+    usernameElement.textContent = username;
+} 
+
+const passvalidation = localStorage.getItem('pass');
+const mailvalidation = localStorage.getItem('Email');
+
+console.log(mailvalidation);
+console.log(passvalidation);
+
 document.addEventListener("DOMContentLoaded", function(){
     document.getElementById("autos").addEventListener("click", function() {
         localStorage.setItem("catID", 101);
@@ -12,3 +26,18 @@ document.addEventListener("DOMContentLoaded", function(){
         window.location = "products.html"
     });
 });
+
+
+
+document.getElementById('closeSession').addEventListener('click', function() {
+    localStorage.clear();
+    alert('Haz cerrado sesion correctamente');
+    window.location.href = 'login.html';
+    
+    event.stopPropagation();
+})
+
+if(passvalidation === null || passvalidation === '' ||  mailvalidation === 'undefined' || mailvalidation === null){
+    alert('Necesitas inicar sesion');
+    window.location.href = 'login.html';
+}
